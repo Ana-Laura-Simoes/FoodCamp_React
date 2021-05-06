373f206bbb4650332dd47e0e4653c6baa8213f75
Ana Laura <ana.simoes-matos@unesp.br>
2021-05-05 22:16:16 -0300

    Contador

--- src/Opcoes.js
+++ src/Opcoes.js
@@ -2,16 +2,21 @@ import React from 'react';
 export default function Opcao(props){
   const [borda, setBorda] = React.useState(false); 
   const [array, setArray] = React.useState(props.array);
+  const [contador, setContador] = React.useState(0); 
 
   //setArray(array.forEach((item) => item.borda=borda));
   function Selecionado(){
    if(!borda){
     setBorda(true);
+    setContador(1);
    }
-      
-      
-      
-  
+  }
+
+  function Decrementar(cont, borda){
+    (cont>1)?setContador(contador-1):setBorda(false);
+  }
+  function Incrementar(cont){
+    setContador(contador+1);
   }
 
     return(
@@ -21,7 +26,7 @@ export default function Opcao(props){
         <div class="descricao">{props.descricao}</div>
         <div class="preco">R$ {props.preco}</div>
         <div class="check">
-          <ion-icon name="checkmark-circle"></ion-icon>
+         <span class="menos" onClick={()=>Decrementar(contador)}>- </span> {contador} <span class="mais" onClick={()=>Incrementar(contador)}> +</span>
         </div>
       </div>
 
